540e52b9de351bc74d98dc68af1e852e0e77e93c
xsobon02 <xsobon02@stud.feec.vutbr.cz>
2021-02-25 14:42:25 +0100

    Add project files.

---
+++ Matrix.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv3
+{
+    public class Matrix
+    {
+        private double[,] matx;
+
+        public Matrix(double[,] matx)
+        {
+            this.matx = matx;
+        }
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            try
+            {
+                var mat1 = new Matrix(new double[a.matx.GetLength(0), a.matx.GetLength(1)]);
+                for (int i = 0; i < a.matx.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.matx.GetLength(1); j++)
+                    {
+                        mat1.matx[i, j] = a.matx[i, j] + b.matx[i, j];
+                    }
+                }
+                return mat1;
+            }
+            catch
+            {
+                throw new Exception("Matrix dimensions aren't acceptable.");
+            }
+
+        }
+        public static Matrix operator -(Matrix a, Matrix b)
+        {
+            try
+            {
+                var mat1 = new Matrix(new double[a.matx.GetLength(0), a.matx.GetLength(1)]);
+                for (int i = 0; i < a.matx.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.matx.GetLength(1); j++)
+                    {
+                        mat1.matx[i, j] = a.matx[i, j] - b.matx[i, j];
+                    }
+                }
+                return mat1;
+            }
+            catch
+            {
+                throw new Exception("Matrix dimensions aren't acceptable.");
+            }
+
+        }
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+
+            try
+            {
+                var mat1 = new Matrix(new double[a.matx.GetLength(0), b.matx.GetLength(1)]);
+                for (int i = 0; i < a.matx.GetLength(0); i++)
+                {
+                    for (int j = 0; j < b.matx.GetLength(1); j++)
+                    {
+                        mat1.matx[i, j] = 0;
+                        for (int k = 0; k < a.matx.GetLength(1); k++)
+                        {
+                            mat1.matx[i, j] += a.matx[i, k] * b.matx[k, j];
+                        }
+                    }
+                }
+                return mat1;
+            }
+            catch
+            {
+                throw new Exception("Matrix dimensions aren't acceptable.");
+            }
+
+
+        }
+        public static bool operator ==(Matrix a, Matrix b)
+        {
+            return a.matx.Equals(b.matx);
+            /*try
+            {
+               for (int i = 0; i < a.matx.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.matx.GetLength(1); j++)
+                    {
+                        if(a.matx[i,j] != b.matx[i,j])
+                        {
+                            return false;
+                        }
+                    }
+                }
+              
+            }
+            catch
+            {
+                return false;
+            }
+            return true;*/
+        }
+        public static bool operator !=(Matrix a, Matrix b)
+        {
+            return !(a.matx.Equals(b.matx));
+            /*try
+            {
+                for (int i = 0; i < a.matx.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.matx.GetLength(1); j++)
+                    {
+                        if (a.matx[i, j] == b.matx[i, j])
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+            }
+            catch
+            {
+                return false;
+            }
+            return true;*/
+        }
+        public static Matrix operator -(Matrix a)
+        {
+            var mat1 = new Matrix(new double[a.matx.GetLength(0), a.matx.GetLength(1)]);
+            for (int i = 0; i < a.matx.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.matx.GetLength(1); j++)
+                {
+                    mat1.matx[i, j] = a.matx[i, j] * (-1);
+                }
+            }
+            return mat1;
+        }
+        public double Determinant()
+        {
+            try
+            {
+                if (matx.GetLength(0) == matx.GetLength(1) && matx.GetLength(1) == 1)
+                {
+                    return matx[0, 0];
+                }
+                else if (matx.GetLength(0) == matx.GetLength(1) && matx.GetLength(1) == 2)
+                {
+                    return matx[0, 0] * (matx[1, 1] - matx[0, 1] * matx[1, 0]);
+
+                }
+                else if (matx.GetLength(0) == matx.GetLength(1) && matx.GetLength(1) == 3)
+                {
+                    var determinant = matx[0, 0] * (matx[1, 1] * matx[2, 2] - matx[1, 2] * matx[2, 1]) -
+                                      matx[0, 1] * (matx[1, 0] * matx[2, 2] - matx[1, 2] * matx[2, 0]) +
+                                      matx[0, 2] * (matx[1, 0] * matx[2, 1] - matx[1, 1] * matx[2, 0]);
+
+                    return determinant;
+                }
+            }
+            catch
+            {
+                throw new Exception("Matrix dimensions aren't acceptable.");
+            }
+            return 0;
+        }
+        public override string ToString()
+        {
+            string output = "";
+
+            for (int i = 0; i < matx.GetLength(0); i++)
+            {
+                for (int j = 0; j < matx.GetLength(1); j++)
+                {
+                    if (j != matx.GetLength(0) - 1) output += $"{matx[i, j]},";
+                }
+                output += "\n";
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] mat1 = new double[,] 
+            {
+                { 1, 2, 3 },
+                { 3, 4, 5 },
+                { 3, 7, 8 }
+
+
+            };
+            double[,] mat2 = new double[,] 
+            {
+                { 5, 4, 2 },
+                { 3, 4, 2 },
+                { 1, 3, 6 }
+            };
+
+            Matrix m1 = new Matrix(mat1);
+            Matrix m2 = new Matrix(mat2);
+
+            Console.WriteLine("Addition:");
+            Console.WriteLine(m1 + m2);
+            Console.WriteLine("Subtraction:");
+            Console.WriteLine(m1 - m2);
+            Console.WriteLine("Multiplication:");
+            Console.WriteLine(m1 * m2);
+            Console.WriteLine("Equal:");
+            Console.WriteLine(m1 == m2);
+            Console.WriteLine("");
+            Console.WriteLine("Nonequal:");
+            Console.WriteLine(m1 != m2);
+            Console.WriteLine("");
+            Console.WriteLine("Determinant:");
+            Console.WriteLine(m1.Determinant());
+            Console.ReadLine();
+        }
+    }
+}
